6ae754bea0145369462964217877ba2766a730ff
chuilishi <108377487+chuilishi@users.noreply.github.com>
2023-12-10 14:40:11 +0800

    六边形网格已完成

--- HexMap/HexMap.cs
+++ HexMap/HexMap.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using EasyButtons;
@@ -21,39 +22,51 @@ public class HexMap : MonoBehaviour
 {
     public GameObject Circle;
 
-    public Vector3Int hexCoordinate;
     //定义一个Vector3Int 数组
     public Vector3Int[] hexCoordinates = new []
     {
         new Vector3Int()
     };
-    
     //Edge Length of every Grid
     public float size;
     private static float innerRadius = 0.866025f/2;
     private static float outerRadius = 1f/2;
-    //列间距
-    private static float ColumnLength = 2*innerRadius;
-    //行间距
-    private static float RowLength = 1.5f * outerRadius;
-    /// <summary>
-    /// 内径/外径  3^0.5 / 4 
-    /// </summary>
-    public const float InnerToOutter = 0.8660254037844386f;
- 
-    /// <summary>
-    /// 外径/内径  4 / 3^0.5 
-    /// </summary>
-    public const float OutterToInner = 1.154700538379252f;
-    [Button]
-    public float3 GetPos()
-    {
-        float y = (hexCoordinate.y - hexCoordinate.z) * Mathf.Sqrt(3) / 2 * outerRadius;
-        float x = hexCoordinate.x * 1.5f * outerRadius;
-        // float x = hexCoordinate.x * 1.5f * outerRadius;
-        // float y = (hexCoordinate.z-hexCoordinate.y) * 1.5f * outerRadius / 2 * 0.866025f;
-        Instantiate(Circle,new Vector3(x,y,0),quaternion.identity);
-        return new float3(x, y, 0);
-    }
     
+    //根据立体坐标获取Unity坐标
+    //x,y,z 分别对应q,s(左上),r
+    public Vector3 GetUnityPos(Vector3 hexCoordinate)
+    {
+        float unityY = (hexCoordinate.y - hexCoordinate.z) * Mathf.Sqrt(3) / 2 * outerRadius;
+        float unityX = hexCoordinate.x * 1.5f * outerRadius;
+        return new Vector3(unityX, unityY, 0);
+    }
+    /// <summary>
+    /// 根据Unity坐标获取
+    /// </summary>
+    /// <returns></returns>
+    public Vector3 GetHexPos(Vector3 unityPos)
+    {
+        var q = unityPos.x * 2 / 3 / outerRadius;
+        var s = (-1f / 3 * unityPos.x + Mathf.Sqrt(3) / 3 * unityPos.y)/outerRadius;
+        var r = -q - s;
+        return new Vector3(q, s, r);
+    }
+    public Vector3 GetNearestUnityPos(float3 pos)
+    {
+        var hexpos = GetHexPos(pos);
+        var a = Mathf.RoundToInt(hexpos.x);
+        var b = Mathf.RoundToInt(hexpos.y);
+        var c = Mathf.RoundToInt(hexpos.z);
+        return GetUnityPos(new Vector3(a, b, c));
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            float3 pos = new float3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x,
+                Camera.main.ScreenToWorldPoint(Input.mousePosition).y,0);
+            Instantiate(Circle, GetNearestUnityPos(pos),quaternion.identity);
+        }
+    }
 }
